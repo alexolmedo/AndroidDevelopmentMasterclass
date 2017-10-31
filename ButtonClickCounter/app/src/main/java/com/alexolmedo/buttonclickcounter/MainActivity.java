93e5217c229c197a2d20f596a1bb8b009a06c932
Alexander Olmedo <olmedo2408@gmail.com>
2017-10-30 20:56:16 -0500

    Button Click and OnClickListener

--- ButtonClickCounter/app/src/main/java/com/alexolmedo/buttonclickcounter/MainActivity.java
+++ ButtonClickCounter/app/src/main/java/com/alexolmedo/buttonclickcounter/MainActivity.java
@@ -1,7 +1,8 @@
 package com.alexolmedo.buttonclickcounter;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -11,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     private EditText userInput;
     private Button button;
     private TextView textView;
+    private int numTimesClicked = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +22,15 @@ public class MainActivity extends AppCompatActivity {
         userInput = (EditText) findViewById(R.id.editText);
         button = (Button) findViewById(R.id.button);
         textView = (TextView) findViewById(R.id.textView);
+        View.OnClickListener ourOnClickedListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                numTimesClicked = numTimesClicked +1;
+                String result = "\nEl botón fue presionado " + numTimesClicked + " veces";
+                textView.append(result);
+
+            }
+        };
+        button.setOnClickListener(ourOnClickedListener);
     }
 }
