2b7c9af4144e3b95279411e0fe8771d342e85692
Alexander Olmedo <olmedo2408@gmail.com>
2017-11-05 10:19:57 -0500

    Hello World Java

---
+++ LearnJava/app/src/main/java/com/alexolmedo/learnjava/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.alexolmedo.learnjava;
+
+/**
+ * Created by Alexander on 5/11/2017.
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("My first Java Program");
+    }
+}
