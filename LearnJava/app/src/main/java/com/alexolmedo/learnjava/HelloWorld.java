ecb06aad7e853bff9997dfd93e0d0ee4f97a0c99
Alexander Olmedo <olmedo2408@gmail.com>
2017-11-08 14:50:04 -0500

    Strings and Ints

--- LearnJava/app/src/main/java/com/alexolmedo/learnjava/HelloWorld.java
+++ LearnJava/app/src/main/java/com/alexolmedo/learnjava/HelloWorld.java
@@ -8,5 +8,14 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("My first Java Program");
+
+        String alex;
+        alex = "Alexander Olmedo";
+        System.out.println(alex);
+
+        int alexSalary = 32;
+        int monthly = alexSalary * 4;
+        System.out.println(alexSalary);
+        System.out.println(monthly);
     }
 }
