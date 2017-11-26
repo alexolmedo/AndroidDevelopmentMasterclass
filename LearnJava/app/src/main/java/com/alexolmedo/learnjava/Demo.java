7f73be820b3badfe66d7c302235f95db04cbbece
Alexander Olmedo <olmedo2408@gmail.com>
2017-11-26 13:39:37 -0500

    Repeating Code While

--- LearnJava/app/src/main/java/com/alexolmedo/learnjava/Demo.java
+++ LearnJava/app/src/main/java/com/alexolmedo/learnjava/Demo.java
@@ -9,7 +9,18 @@ public class Demo {
     public static void main(String[] args) {
         VampyreKing dracula = new VampyreKing("Dracula");
         dracula.showInfo();
-        dracula.takeDamage(12);
-        dracula.showInfo();
+
+        while(dracula.getLives() > 0) {
+            if(dracula.dodges()){
+                continue;
+            }
+            if(dracula.runAway()) {
+                System.out.println("Dracula ran away");
+                break;
+            } else {
+                dracula.takeDamage(12);
+                dracula.showInfo();
+            }
+        }
     }
 }
--- LearnJava/app/src/main/java/com/alexolmedo/learnjava/VampyreKing.java
+++ LearnJava/app/src/main/java/com/alexolmedo/learnjava/VampyreKing.java
@@ -1,5 +1,7 @@
 package com.alexolmedo.learnjava;
 
+import java.util.Random;
+
 /**
  * Created by Alexander on 26/11/2017.
  */
@@ -14,5 +16,24 @@ public class VampyreKing extends Vampyre {
     @Override
     public void takeDamage(int damage) {
         super.takeDamage(damage / 2);
+    }
+
+    public boolean runAway() {
+//        if (getLives() < 2) {
+//            return true;
+//        } else {
+//            return false;
+//        }
+        return (getLives() < 2);
+    }
+
+    public boolean dodges() {
+        Random rand = new Random();
+        int chance = rand.nextInt(6);
+        if(chance > 3) {
+            System.out.println("Dracula dodges");
+            return true;
+        }
+        return false;
     }
 }
