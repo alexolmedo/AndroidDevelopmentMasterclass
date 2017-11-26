2c6216ba1a141746a7662e1d1cc382e262157ceb
Alexander Olmedo <olmedo2408@gmail.com>
2017-11-25 22:42:40 -0500

    Extending from a class.

---
+++ LearnJava/app/src/main/java/com/alexolmedo/learnjava/Troll.java
@@ -0,0 +1,11 @@
+package com.alexolmedo.learnjava;
+
+/**
+ * Created by Alexander on 25/11/2017.
+ */
+
+public class Troll extends Enemy {
+    public Troll(String name) {
+        super(name, 27, 1);
+    }
+}
