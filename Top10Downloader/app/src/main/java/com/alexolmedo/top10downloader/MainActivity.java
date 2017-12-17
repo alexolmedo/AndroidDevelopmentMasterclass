d8ca6aac0f64bbb617cb49ac99353b1b22ee443b
Alexander Olmedo <olmedo2408@gmail.com>
2017-12-17 10:49:53 -0500

    doInBackround and downloadXML

--- Top10Downloader/app/src/main/java/com/alexolmedo/top10downloader/MainActivity.java
+++ Top10Downloader/app/src/main/java/com/alexolmedo/top10downloader/MainActivity.java
@@ -5,6 +5,13 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
 
@@ -26,13 +33,34 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
-            Log.d(TAG, "onPostExecute: parameter is " + s );
+            Log.d(TAG, "onPostExecute: parameter is " + s);
         }
 
         @Override
         protected String doInBackground(String... strings) {
             Log.d(TAG, "doInBackground: starts with " + strings[0]);
-            return "doInBackround completed.";
+            String rssFeed = downloadXML(strings[0]);
+            if (rssFeed == null) {
+                Log.e(TAG, "doInBackground: Error downloading");
+
+            }
+            return rssFeed;
+        }
+
+        private String downloadXML(String urlPath){
+            StringBuilder xmlResult = new StringBuilder();
+
+            try {
+                URL url = new URL(urlPath);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                int response = connection.getResponseCode();
+                Log.d(TAG, "downloadXML: The response code was " + response);
+                InputStream inputStream = connection.getInputStream();
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader reader = new BufferedReader(inputStreamReader);
+            } catch (MalformedURLException e ){
+                Log.e(TAG, "downloadXML: Invalid URL " + e.getMessage());
+            }
         }
     }
 
